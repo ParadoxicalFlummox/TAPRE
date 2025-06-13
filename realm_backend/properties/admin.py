f91d3a758805b4826055a10cc8147e93bf7fdd85
ParadoxicalFlummox <adamr814@protonmail.com>
2025-06-13 17:39:09 +0000

    Redo file structure

--- realm_backend/properties/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Property
-
-@admin.register(Property)
-class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('address', 'city', 'state', 'zip_code', 'country', 'construction_date', 'last_inspection_date', 'created_at', 'updated_at')
-    search_fields = ('address', 'city', 'state', 'zip_code')
--- realm_backend/receipts/admin.py
+++
@@ -1,7 +0,0 @@
-from .models import Receipt
-from django.contrib import admin
-
-@admin.register(Receipt)
-class ReceiptAdmin(admin.ModelAdmin):
-    list_display = ('transaction', 'uploaded_at', 'description')
-    search_fields = ('description',)
